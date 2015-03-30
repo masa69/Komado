15106be60097fe6d45bb70cd2e2a2a40b578b139
pipo <psychedelic.nekopunch@gmail.com>
2015-03-30 23:08:43 +0900

    API参照系のリストを整理

--- app.rb
+++ app.rb
@@ -21,9 +21,24 @@ get '/player/:id' do
 	erb :index
 end
 
-
 # API
 # ================
+
+def pushToLists(id, type, thumbnail, title)
+	res = {
+		'id' => id,
+		'type' => type,
+		'thumbnail' => thumbnail,
+		'title' => title,
+	}
+	res
+end
+
+def initYoutubeApi
+	client  = Google::APIClient.new(:key => DEVELOPER_KEY, :authorization => nil)
+	youtube = client.discovered_api(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION)
+	return client, youtube
+end
 
 get '/api/youtube/find' do
 
@@ -37,41 +52,39 @@ get '/api/youtube/find' do
 			opt :maxResults, 'Max results', :type => :int, :default => 25
 		end
 
-		client  = Google::APIClient.new(:key => DEVELOPER_KEY,
-		                                :authorization => nil)
-		youtube = client.discovered_api(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION)
+		client, youtube = initYoutubeApi
+
 		# Call the search.list method to retrieve results matching the specified
 		# query term.
+
 		opts[:part] = 'id,snippet'
+
 		search_response = client.execute!(
 			:api_method => youtube.search.list,
 			:parameters => opts
 		)
 
-		videos    = []
-		channels  = []
-		playlists = []
 		res = []
 
-		# search_response.data.items.each do |search_result|
-		# 	case search_result.id.kind
-		# 		when 'youtube#video'
-		# 			videos.push("#{search_result.snippet.title} (#{search_result.id.videoId})")
-		# 		when 'youtube#channel'
-		# 			channels.push("#{search_result.snippet.title} (#{search_result.id.channelId})")
-		# 		when 'youtube#playlist'
-		# 			playlists.push("#{search_result.snippet.title} (#{search_result.id.playlistId})")
-		# 	end
-		# end
+		search_response.data.items.each do |search_result|
+			case search_result.id.kind
+				when 'youtube#video'
+					res.push(pushToLists(
+						search_result.id.videoId, 'yt_video',
+						search_result.snippet.thumbnails.medium.url,
+						search_result.snippet.title))
+				when 'youtube#playlist'
+					res.push(pushToLists(
+						search_result.id.playlistId, 'yt_playlist',
+						search_result.snippet.thumbnails.medium.url,
+						search_result.snippet.title))
+			end
+		end
 
-		# # res.push("Videos:\n" + videos + "\n", "Channels:\n" + channels + "\n", "Playlists:\n" + playlists + "\n")
-		# videos
 		status 200
-		json search_response.data.items, :content_type => :js
-
+		json res, :content_type => :js
 	rescue Google::APIClient::TransmissionError => e
 		status 500
 		e.result.body
 	end
-
 end
