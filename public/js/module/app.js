0ce6ebd2a85cd0fc58eacb905ddc3b47578e2ff6
pipo <psychedelic.nekopunch@gmail.com>
2015-05-10 02:29:29 +0900

    ルーティングをapp.jsに移動

--- public/js/module/app.js
+++ public/js/module/app.js
@@ -1,1 +1,27 @@
-var app = angular.module('komado', ['ngSanitize', 'ngAnimate', 'ngRoute', 'youtube-embed']);
+var app = angular.module('komado', ['ngSanitize', 'ngAnimate', 'ngRoute', 'ngCookies', 'angularMoment', 'youtube-embed']);
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider)
+{
+	$routeProvider
+		.when('/', {
+			templateUrl: '/templates/views/index.html',
+			controller: 'IndexController',
+			controllerAs: 'page',
+		})
+		.when('/:id', {
+			templateUrl: '/templates/views/user.html',
+			controller: 'UserController',
+			controllerAs: 'page',
+		})
+		.when('/player/:id/', {
+			templateUrl: '/templates/views/player.html',
+			controller: 'PlayerController',
+			controllerAs: 'page',
+		})
+		.when('/player/:id/:videoId', {
+			templateUrl: '/templates/views/player.html',
+			controller: 'PlayerController',
+			controllerAs: 'page',
+		});
+	$locationProvider.html5Mode(true);
+}]);
