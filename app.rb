6744307de11ab55b7bb7e3cfeb9922acffad2f89
pipo <psychedelic.nekopunch@gmail.com>
2015-05-24 22:31:04 +0900

    history api

--- app.rb
+++ app.rb
@@ -84,3 +84,153 @@ get '/api/youtube/find' do
 		e.result.body
 	end
 end
+
+get '/api/youtube/history/get/:user' do
+
+	user = params['user']
+
+	begin
+		if !user
+			raise 'user is required'
+		end
+		dc     = Dalli::Client.new('localhost:11211')
+		videos = dc.get('history_' + user)
+
+		videos.each_index do |i|
+			id = i + 1
+			videos[i]['id'] = id.to_s
+		end
+
+		status 200
+		res = {
+			'result'  => 'success',
+			'message' => '',
+			'data'    => videos,
+		}
+		json res, :content_type => :js
+	rescue Exception => e
+		status 500
+		res = {
+			'result'  => 'failed',
+			'message' => e,
+			'data'    => '',
+		}
+	end
+end
+
+get '/api/youtube/history/add/:user' do
+
+	MAX_ITEMS = 30
+
+	user      = params['user']
+	videoId   = params[:video_id]
+	type      = params[:type]
+	thumbnail = params[:thumbnail]
+	title     = params[:title]
+
+	# videoId   = '1'
+	# type      = 'type'
+	# thumbnail = 'thumbnail'
+	# title     = 'title'
+
+	begin
+		if !user
+			raise 'user is required'
+		end
+		if !videoId
+			raise 'video_id is required'
+		end
+		if !type
+			raise 'type is required'
+		end
+		if !thumbnail
+			raise 'thumbnail is required'
+		end
+		if !title
+			raise 'title is required'
+		end
+
+		videoData = {
+			'videoId'   => videoId,
+			'type'      => type,
+			'thumbnail' => thumbnail,
+			'title'     => title,
+		}
+
+		options = {:expires_in => 60*60*24*7}
+		dc = Dalli::Client.new('localhost:11211', options)
+		videos = dc.get('history_' + user)
+
+		if videos.kind_of? (Array)
+			videos.reverse!
+			sub    = Array[videoData]
+			videos = videos - sub
+			videos << videoData
+			videos.reverse!
+		else
+			videos = Array[videoData]
+		end
+
+		if videos.length > MAX_ITEMS
+			videos.slice!(MAX_ITEMS - 1, videos.length - MAX_ITEMS)
+		end
+
+		dc.set('history_' + user, videos)
+
+		status 200
+		res = {
+			'result'  => 'success',
+			'message' => '',
+			'data'    => '',
+		}
+		json res, :content_type => :js
+	rescue Exception => e
+		status 500
+		res = {
+			'result'  => 'failed',
+			'message' => e,
+			'data'    => '',
+		}
+		json res, :content_type => :js
+	end
+end
+
+get '/api/youtube/history/delete/:user' do
+
+	user = params['user']
+	id   = params[:list_id]
+
+	begin
+		if !user
+			raise 'user is required'
+		end
+		if !id
+			raise 'list_id is required'
+		end
+
+		dc = Dalli::Client.new('localhost:11211')
+		videos = dc.get('history_' + user)
+
+		if videos.kind_of? (Array)
+			videos.slice!(id)
+		end
+
+		dc.set('history_' + user, videos)
+
+		status 200
+		res = {
+			'result'  => 'success',
+			'message' => '',
+			'data'    => '',
+		}
+		json res, :content_type => :js
+	rescue Exception => e
+		status 500
+		res = {
+			'result'  => 'failed',
+			'message' => e,
+			'data'    => '',
+		}
+		json res, :content_type => :js
+	end
+end
