463c52d4f9a906e80b5714804beb2b1ab0129009
pipo <psychedelic.nekopunch@gmail.com>
2015-06-01 23:04:25 +0900

    授業動画の再生履歴

--- public/js/controllers/PlayerController.js
+++ public/js/controllers/PlayerController.js
@@ -1,32 +1,70 @@
-app.controller('PlayerController', function($scope, User, Player, playerControl, videoSearchBar, videoSearchList)
+app.controller('PlayerController', function($scope, User, Player, playerControl, videoSearchBar, videoSearchList, videoHistoryList)
 {
 	'use strict';
 
 	var self = this;
 
-	self.videoSearchBar  = videoSearchBar;
-	self.videoSearchList = videoSearchList;
-	self.playerControl   = playerControl;
+	self.videoSearchBar   = videoSearchBar;
+	self.videoSearchList  = videoSearchList;
+	self.videoHistoryList = videoHistoryList;
+	self.playerControl    = playerControl;
+
+	self.list = {
+		search   : true,
+		history  : false,
+		favorite : false,
+	};
 
 	self.init = function()
 	{
 		User.init();
 	};
 
+	self.switchList = function(type)
+	{
+		angular.forEach(self.list, function(val, key)
+		{
+			self.list[key] = false;
+		});
+		self.list[type] = true;
+
+		if (self.list.history) {
+			self.videoHistoryList.initList();
+		}
+	};
+
+
+
 	$scope.$root.$on('user:init', function()
 	{
 		Player.init();
 		self.videoSearchBar.init();
 		self.videoSearchList.init();
+		self.videoHistoryList.init();
 	});
+
+
 
 	$scope.$root.$on('player:init', function()
 	{
 		self.playerControl.init();
 	});
 
+	$scope.$root.$on('player:set:setting', function()
+	{
+		self.playerControl.refreshSetting();
+	});
+
+	$scope.$root.$on('player:set:videoid', function()
+	{
+		self.playerControl.refreshVideoId();
+	});
+
+
+
 	$scope.$root.$on('video:find', function()
 	{
+		self.switchList('search');
 		self.videoSearchList.refreshList();
 	});
 
@@ -35,15 +73,39 @@ app.controller('PlayerController', function($scope, User, Player, playerControl,
 
 	});
 
-	$scope.$root.$on('player:set:videoid', function()
+
+
+	$scope.$root.$on('videohistory:getlist', function()
 	{
-		self.playerControl.refreshVideoId();
+		self.videoHistoryList.refreshList();
 	});
 
-	$scope.$root.$on('player:set:setting', function()
+	$scope.$root.$on('videohistory:getlist:error', function()
 	{
-		self.playerControl.refreshSetting();
+
 	});
+
+	$scope.$root.$on('videohistory:add', function()
+	{
+		self.videoHistoryList.initList();
+	});
+
+	$scope.$root.$on('videohistory:add:error', function()
+	{
+
+	});
+
+	$scope.$root.$on('videohistory:delete', function()
+	{
+		self.videoHistoryList.initList();
+	});
+
+	$scope.$root.$on('videohistory:delete:error', function()
+	{
+
+	});
+
+
 
 	$scope.$on('youtube.player.ready', function($event, player)
 	{
